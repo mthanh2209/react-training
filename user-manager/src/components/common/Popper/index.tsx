be85b37310bd8ca8ec5d376c7b694130cbad1b99
mthanh2209 <mongthanh.220902@gmail.com>
2023-12-06 15:10:20 +0700

    Update popper component #6

--- user-manager/src/components/common/Popper/index.tsx
+++ user-manager/src/components/common/Popper/index.tsx
@@ -1,69 +1,43 @@
-import { useEffect, useRef, useState } from 'react';
-import { createPortal } from 'react-dom';
-
 import '@components/common/Popper/Popper.css';
 import { IPopperProps } from '@types/interfaces';
+import usePopper from '@helpers/popper';
+import { useEffect } from 'react';
 
-const Popper = ({
-  open,
-  icon,
-  children,
-  options
-}: IPopperProps) => {
-  const [showOption, setShowOption] = useState(open)
-  const ref = useRef<HTMLButtonElement>(null);
-
-  const handleShowOption = () => {
-    setShowOption(true)
-  }
+const Popper = ({ open = false, icon, children, options }: IPopperProps) => {
+  const { ref, isOpen, setIsOpen, handleTogglePopper } = usePopper();
 
   useEffect(() => {
-    setShowOption(open);
+    setIsOpen(open);
   }, [open]);
-
-  useEffect(() => {
-    const handleCloseOption = (event: MouseEvent) => {
-      if (!ref.current?.contains(event.target as Node)) {
-        setShowOption(false);
-      }
-    };
-    document.addEventListener('click', handleCloseOption);
-    return () => {
-      document.removeEventListener('click', handleCloseOption);
-    };
-  }, []);
 
   return (
     <div className='popper'>
       <button
         className='btn-primary btn-popper'
         type='button'
-        onClick={handleShowOption}
-        ref={ref}>
-        <img
-          className='plus-icon'
-          alt={`${children}-icon`}
-          src={icon} />
+        onClick={handleTogglePopper}
+        ref={ref as React.RefObject<HTMLButtonElement>}
+      >
+        <img className='plus-icon' alt={`${children}-icon`} src={icon} />
         {children}
       </button>
 
-      {showOption &&
-        createPortal(
-          <div className='popper-option'>
-            {options.map((option, index) => (
-              <button
-                key={index}
-                className='btn-popper btn-option'
-                type='button'
-                onClick={option.onClick}>
-                {option.text}
-              </button>
-            ))}
-          </div>,
-          document.querySelector('.popper') as HTMLElement
-        )}
+      {isOpen && (
+        <div className='popper-option'>
+          {options.map((option, index) => (
+            <button
+              key={index}
+              className='btn-popper btn-option'
+              type='button'
+              onClick={option.onClick}
+            >
+              {option.text}
+            </button>
+          ))}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default Popper;
