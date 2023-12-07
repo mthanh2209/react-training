06f75d73734e3bfc150f1a617d8e9eb5ba94a17c
mthanh2209 <mongthanh.220902@gmail.com>
2023-12-07 16:56:24 +0700

    Implement ListNav Component #60

---
+++ user-manager/src/components/common/ListNav/ListNav.stories.tsx
@@ -0,0 +1,38 @@
+import ListNav from '@components/common/ListNav/index';
+import type { Meta } from '@storybook/react';
+import userIcon from '@assets/images/user-icon.svg';
+import { useState } from 'react';
+
+export default {
+  title: 'Components/ListNav',
+  component: ListNav
+} as Meta;
+
+type ListNavStoryArgs = {
+  items: {
+    content?: string;
+    icon?: string;
+    onClick: () => void }[];
+};
+
+export const Default = ({
+  items
+}: ListNavStoryArgs) => {
+  const [selected, setSelected] = useState<number>(0);
+
+  return (
+    <ListNav
+      items={items}
+      selected={selected}
+      onClick={(index: number) => setSelected(index)}
+    />
+  );
+};
+
+Default.args = {
+  items: [
+    { content: 'Users', icon: userIcon, onClick: () => {} },
+    { content: 'Roles', icon: userIcon, onClick: () => {} },
+    { content: 'Rules', icon: userIcon, onClick: () => {} }
+  ]
+};
