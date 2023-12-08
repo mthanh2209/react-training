3819af6d2f43ee7540556a4b28468ea4ae5a0178
mthanh2209 <mongthanh.220902@gmail.com>
2023-12-08 10:47:24 +0700

    Move interface of popper option #11

--- user-manager/src/types/interface.ts
+++ user-manager/src/types/interface.ts
@@ -1,6 +1,11 @@
+export interface IPopperOption {
+  text?: string;
+  onClick?: () => void;
+}
+
 export interface IItemNav {
   icon?: string;
   content?: string;
   selected?: boolean;
-  onClick: () => void;
+  onClick?: () => void;
 }
