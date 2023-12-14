a2261ed861aa5cb4d5cea5c80714d04a4c80b2ba
mthanh2209 <mongthanh.220902@gmail.com>
2023-12-14 16:32:42 +0700

    Add helper for Table Component #16

--- user-manager/src/helpers/index.ts
+++ user-manager/src/helpers/index.ts
@@ -16,3 +16,10 @@ export const convertToDataURL = (file?: File) => {
     };
   });
 };
+
+export const getObjValue = <T, Key extends keyof T>(
+  obj: T,
+  key: Key
+): T[Key] => {
+  return obj[key];
+};
