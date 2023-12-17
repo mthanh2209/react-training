3becb448d2135969f7dc532f88a4abc1c38a8d0e
mthanh2209 <mongthanh.220902@gmail.com>
2023-12-18 00:42:35 +0700

    Implement Profile Editor for Panel Component #24

---
+++ user-manager/src/components/Inputs/Panel/ProfileEditor/index.tsx
@@ -0,0 +1,195 @@
+import Modal from '@components/DataDisplay/Modal';
+import Status from '@components/DataDisplay/Status';
+import Button from '@components/Inputs/Button';
+import ImageUploader from '@components/Inputs/ImageUploader';
+import SwitchStatus from '@components/Inputs/SwitchStatus';
+import TextArea from '@components/Inputs/TextArea';
+import TextField from '@components/Inputs/TextField';
+import { renderDate } from '@helpers';
+import { IUserProps } from '@types/interface';
+import { useEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
+
+interface IProfileEditor<T> {
+  activeItemBar: string;
+  contentItem?: string;
+  itemData: T;
+  onSaveUser: (itemData: IUserProps) => void;
+  onDeleteUser: (id: number) => void;
+}
+const ProfileEditor = ({
+  activeItemBar,
+  contentItem = 'General',
+  itemData,
+  onSaveUser,
+  onDeleteUser
+}: IProfileEditor<IUserProps>) => {
+  const [avatar, setAvatar] = useState(itemData.avatar);
+  const [fullName, setFullName] = useState(itemData.fullName);
+  const [email, setEmail] = useState(itemData.email);
+  const [status, setStatus] = useState(itemData.isActive);
+  const [details, setDetails] = useState(itemData.details);
+  const [isOpenModal, setOpenModal] = useState(false);
+
+  const handleFullNameChange = (value: string) => {
+    setFullName(value);
+  };
+
+  const handleEmailChange = (value: string) => {
+    setEmail(value);
+  };
+
+  const handleSwitchChange = () => {
+    setStatus(!status);
+  };
+
+  const handleDetailsChange = (value: string) => {
+    setDetails(value);
+  };
+
+  const handleAvatarChange = (value: string) => {
+    setAvatar(value);
+  };
+
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const date = new Date();
+    const currentDate = date.toString();
+    const updatedItem = {
+      id: itemData.id,
+      avatar: avatar,
+      fullName: fullName,
+      email: email,
+      isActive: status,
+      registeredDate: itemData.registeredDate,
+      lastVisitedDate: currentDate,
+      details: details,
+      bgColor: itemData.bgColor
+    };
+    onSaveUser(updatedItem);
+  };
+
+  const handleOpenModal = () => {
+    setOpenModal(true);
+  };
+
+  const handleDeleteButton = () => {
+    setOpenModal(false);
+    onDeleteUser(itemData.id);
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+
+  useEffect(() => {
+    setFullName(itemData.fullName);
+    setEmail(itemData.email);
+    setStatus(itemData.isActive);
+    setDetails(itemData.details);
+    setAvatar(itemData.avatar);
+  }, [itemData]);
+
+  switch (activeItemBar) {
+    case contentItem:
+      return (
+        <>
+          <div className='confirm-buttons'>
+            <Button
+              variants='secondary'
+              type='button'
+              content='Delete'
+              onClick={handleOpenModal}
+            />
+            <Button variants='primary' type='submit' content='Save' />
+          </div>
+
+          {isOpenModal &&
+            createPortal(
+              <Modal
+                isOpen={isOpenModal}
+                type='confirm'
+                modalTitle='Delete'
+                modalDesc='Are you sure to delete this user?'
+                confirmText='Delete'
+                denyText='Save'
+                onClose={handleCloseModal}
+                onConfirmText={handleDeleteButton}
+              />,
+              document.querySelector('.popper') as HTMLElement
+            )}
+
+          <form
+            id='form-edit-profile'
+            className='form-edit-profile'
+            onSubmit={handleFormSubmit}
+          >
+            <div className='form-item'>
+              <span className='form-item-title'>Full Name</span>
+              <TextField
+                className='text'
+                value={fullName}
+                onChange={handleFullNameChange}
+              />
+            </div>
+
+            <div className='form-item'>
+              <span className='form-item-title'>Email</span>
+              <TextField
+                className='text'
+                value={email}
+                onChange={handleEmailChange}
+              />
+            </div>
+
+            <div className='form-item form-item-avatar'>
+              <span className='form-item-title'>Avatar</span>
+              <ImageUploader
+                initialImage=''
+                alt={fullName}
+                bgColor={itemData.bgColor}
+                onChange={handleAvatarChange}
+              />
+            </div>
+
+            <div className='form-item form-item-status'>
+              <span className='form-item-title'>Status</span>
+              <SwitchStatus isChecked={status} onChange={handleSwitchChange} />
+
+              <div className='status-wrapper'>
+                <Status isActive={status} />
+              </div>
+            </div>
+
+            <div className='form-item'>
+              <span className='form-item-title'>Registered</span>
+              <p className='form-edit-content'>
+                {itemData.registeredDate == null
+                  ? 'Unknown'
+                  : renderDate(itemData.registeredDate)}
+              </p>
+            </div>
+
+            <div className='form-item'>
+              <span className='form-item-title'>Last visited</span>
+              <p className='form-edit-content'>
+                {itemData.lastVisitedDate == null
+                  ? 'Unknown'
+                  : renderDate(itemData.lastVisitedDate)}
+              </p>
+            </div>
+
+            <div className='form-item form-item-details'>
+              <span className='form-item-title'>Details</span>
+              <TextArea onChange={handleDetailsChange} value={details} />
+            </div>
+          </form>
+        </>
+      );
+
+      default:
+        return null;
+  }
+};
+
+export default ProfileEditor;
