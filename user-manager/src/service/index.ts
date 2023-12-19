233d51c2f2addf0c71fbcd585a3260016f5baabc
mthanh2209 <mongthanh.220902@gmail.com>
2023-12-19 14:27:22 +0700

    Update service for fetching API #87

--- user-manager/src/service/index.ts
+++ user-manager/src/service/index.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // Constants
-import { USERS_URL } from '@constants/urls';
+import { API_URL, USERS_URL } from '@constants/urls';
+import { API_REQUEST } from '@constants';
 
 // Helpers
 import { getRandomColor } from '@helpers';
@@ -9,60 +10,90 @@ import { getRandomColor } from '@helpers';
 // Types
 import { IUserProps as IData } from '@types/interface';
 
-export const get = async () => {
+axios.defaults.baseURL = API_URL;
+
+type IServiceProps = {
+  data: any;
+  error: string | null;
+};
+
+const handleResponse = (data: any): IServiceProps => ({
+  data,
+  error: null
+});
+
+const handleError = (error?: any): IServiceProps => ({
+  error: error
+    ? error.message || 'Something went wrong'
+    : 'Something went wrong',
+  data: null
+});
+
+const makeRequest = async (
+  method: string,
+  url: string,
+  data?: any
+): Promise<IServiceProps> => {
   try {
-    const data = await axios.get(USERS_URL);
-    console.log(data);
+    let response;
+    if (method === API_REQUEST.GET) {
+      response = await axios.get(url);
+    } else if (method === API_REQUEST.POST) {
+      response = await axios.post(url, data);
+    } else if (method === API_REQUEST.PUT) {
+      response = await axios.put(url, data);
+    } else if (method === API_REQUEST.DELETE) {
+      response = await axios.delete(url);
+    }
+
+    if (response && response.data) {
+      return handleResponse(response.data);
+    } else {
+      throw new Error('Empty response');
+    }
   } catch (error) {
-    console.error(error);
+    return handleError(error);
   }
 };
 
-export const post = async (id: number, fullName: string) => {
-  try {
-    const registeredDate = new Date().toISOString();
+export const get = async (): Promise<IServiceProps> =>
+  makeRequest(
+    API_REQUEST.GET,
+    USERS_URL
+  );
 
-    const data = await axios.post(USERS_URL, {
-      id: id,
-      avatar: '',
-      fullName: fullName,
-      email: '',
-      isActive: false,
-      registeredDate: registeredDate,
-      lastVisitedDate: null,
-      details: '',
-      bgColor: getRandomColor()
-    });
-    console.log(data);
-  } catch (error) {
-    console.error(error);
-  }
+export const post = async (
+  id: number,
+  fullName: string
+): Promise<IServiceProps> => {
+  const registeredDate = new Date().toISOString();
+  const postData = {
+    id,
+    avatar: '',
+    fullName,
+    email: '',
+    isActive: false,
+    registeredDate,
+    lastVisitedDate: null,
+    details: '',
+    bgColor: getRandomColor()
+  };
+  return makeRequest(
+    API_REQUEST.POST,
+    USERS_URL,
+    postData
+  );
 };
 
-export const update = async (dataItem: IData) => {
-  try {
-    const data = await axios.put(`${USERS_URL}/${dataItem.id}`, {
-      id: dataItem.id,
-      avatar: dataItem.avatar,
-      fullName: dataItem.fullName,
-      email: dataItem.email,
-      isActive: dataItem.isActive,
-      registeredDate: dataItem.registeredDate,
-      lastVisitedDate: dataItem.lastVisitedDate,
-      details: dataItem.details,
-      bgColor: dataItem.bgColor
-    });
-    console.log(data);
-  } catch (error) {
-    console.error(error);
-  }
-};
+export const update = async (dataItem: IData): Promise<IServiceProps> =>
+  makeRequest(
+    API_REQUEST.PUT,
+    `${USERS_URL}/${dataItem.id}`,
+    dataItem
+  );
 
-export const deleted = async (id: number) => {
-  try {
-    const data = await axios.delete(`${USERS_URL}/${id}`);
-    console.log(data);
-  } catch (error) {
-    console.error(error);
-  }
-};
+export const deleted = async (id: number): Promise<IServiceProps> =>
+  makeRequest(
+    API_REQUEST.DELETE,
+    `${USERS_URL}/${id}`
+  );
