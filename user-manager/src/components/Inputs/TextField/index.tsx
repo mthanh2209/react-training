ae307612ac5ac34f6817229c017ef3df228da55b
mthanh2209 <mongthanh.220902@gmail.com>
2023-12-21 09:17:07 +0700

    Update TextField component #26

--- user-manager/src/components/Inputs/TextField/index.tsx
+++ user-manager/src/components/Inputs/TextField/index.tsx
@@ -7,7 +7,7 @@ import '@components/Inputs/TextField/TextField.css';
 import { TInput } from '@types';
 
 interface ITextFieldProps {
-  label?: string
+  label?: string;
   additionalClass?: TInput;
   value?: string;
   placeholder?: string;
@@ -24,26 +24,31 @@ const TextField = ({
   onChange
 }: ITextFieldProps) => {
   const handleChangeInput = (event: FormEvent<HTMLInputElement>) => {
-    onChange?.(event.currentTarget.value)
-  }
+    onChange?.(event.currentTarget.value);
+  };
 
   return (
     <>
-      <label className={`label-input ${additionalClass}`}>
-        {label}
-      </label>
+      {additionalClass === 'input-search' ||
+      additionalClass === 'input-submit' ? (
+        <label className='label-input hide'>{label}</label>
+      ) : (
+        <label className='label-input'>{label}</label>
+      )}
+
       <div className='input-wrapper'>
         <input
           type='text'
           className={`text-field ${additionalClass}`}
           value={value}
           placeholder={placeholder}
-          onChange={handleChangeInput} />
+          onChange={handleChangeInput}
+        />
 
         <span className='error-message'>{errorText}</span>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default TextField;
