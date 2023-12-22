7bd6c5d47523a006c3d4ecbdc406edd762eac118
mthanh2209 <mongthanh.220902@gmail.com>
2023-12-22 12:00:33 +0700

    Create constant files #30

---
+++ user-manager/src/constants/listNav.ts
@@ -0,0 +1,11 @@
+// Icons
+import userIcon from '@assets/images/user-icon.svg';
+import userIconSelected from '@assets/images/user-icon-selected.svg';
+
+export const ListNav = [
+  {
+    icon: userIcon,
+    iconSelected: userIconSelected,
+    content: 'Users'
+  }
+];
