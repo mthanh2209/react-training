ec2083d2d4882d5e84e46fc67e6b6be556f08a92
mthanh2209 <mongthanh.220902@gmail.com>
2023-12-22 08:51:25 +0700

    Implement edit information user #30

--- user-manager/src/App.tsx
+++ user-manager/src/App.tsx
@@ -7,6 +7,8 @@ import Table from '@components/DataDisplay/Table';
 import Toolbar from '@components/DataDisplay/Toolbar';
 import Status from '@components/DataDisplay/Status';
 import Avatar from '@components/DataDisplay/Avatar';
+import InformationSidebar from '@components/DataDisplay/Sidebar';
+import Panel from '@components/Inputs/Panel';
 
 // Icons
 import userIcon from '@assets/images/user-icon.svg';
@@ -17,8 +19,10 @@ import { IUserProps as IUser } from '@types/interface';
 import { IColumnProps } from '@types/interface';
 
 // Services
-import { addUsers, getUsers } from '@service';
-import InformationSidebar from '@components/DataDisplay/Sidebar';
+import { addUsers, getUsers, updateUsers } from '@service';
+
+// Helpers
+import { formatDate } from '@helpers';
 
 const popperOption = [{ text: 'Add new user' }];
 
@@ -69,7 +73,10 @@ const infoList = (data: IUser | null) => {
     {
       icon: 'date-icon',
       title: 'Last visited:',
-      content: data.lastVisitedDate
+      content:
+        data.lastVisitedDate !== null
+          ? formatDate(data.lastVisitedDate)
+          : 'Unknown'
     }
   ];
 };
@@ -79,16 +86,21 @@ const App = () => {
   const [rowIndex, setRowIndex] = useState(0);
   const [rowData, setRowData] = useState<IUser | null>(null);
   const [userInfoList, setUserInfoList] = useState<any[]>([]);
+  const [showSidebar, setShowSidebar] = useState(true);
+
+  const handleGetUsers = async () => {
+    const response = await getUsers();
+    if (response.data) {
+      setUsers(response.data);
+    }
+  };
 
   useEffect(() => {
-    const handleGetUsers = async () => {
-      const response = await getUsers();
-      if (response.data) {
-        setUsers(response.data);
-      }
-    };
+    if (rowData) {
+      setUserInfoList(infoList(rowData));
+    }
     handleGetUsers();
-  }, []);
+  }, [rowData]);
 
   const handleAddUsers = async (userName: string) => {
     const response = await addUsers(userName);
@@ -97,19 +109,38 @@ const App = () => {
       setUsers(data);
       setRowIndex(data.length);
       setRowData(data[data.length - 1]);
+      setShowSidebar(true);
     }
   };
 
-  useEffect(() => {
-    setUserInfoList(infoList(rowData))
-  }, [rowData]);
+  const handleShowPanel = () => {
+    setShowSidebar(false);
+  };
+
+  const handleClosePanel = () => {
+    setShowSidebar(true);
+  };
+
+  const handleUpdateUsers = async (itemData: IUser) => {
+    const response = await updateUsers(itemData);
+    console.log(response);
+    if (response.data) {
+      setRowData(response.data);
+      handleGetUsers();
+    }
+  };
+
+  const handleDeleteUsers = () => {};
 
   const handleSelectedRow = (index: number, dataItem: IUser): void => {
     setRowData(dataItem);
     setRowIndex(index);
+    if (showSidebar || showSidebar === null) {
+      setShowSidebar(true);
+    } else if (!showSidebar) {
+      setShowSidebar(false);
+    }
   };
-
-  const handleShowPanel = () => {};
 
   return (
     <>
@@ -131,7 +162,7 @@ const App = () => {
           />
         </div>
 
-        {rowData !== null && (
+        {showSidebar && rowData !== null && (
           <InformationSidebar
             title='User information'
             isActive={rowData.isActive}
@@ -140,6 +171,24 @@ const App = () => {
             fullName={rowData.fullName}
             infoList={userInfoList}
             onEditButton={handleShowPanel}
+          />
+        )}
+
+        {!showSidebar && rowData !== null && (
+          <Panel
+            listBar={['General']}
+            id={rowData.id}
+            avatar={rowData.avatar}
+            fullName={rowData.fullName}
+            email={rowData.email}
+            isActive={rowData.isActive}
+            registeredDate={rowData.registeredDate}
+            lastVisitedDate={rowData.lastVisitedDate}
+            details={rowData.details}
+            bgColor={rowData.bgColor}
+            onReturnClick={handleClosePanel}
+            onSaveUser={handleUpdateUsers}
+            onDeleteUser={handleDeleteUsers}
           />
         )}
       </main>
