0e030a48efe9abbe2993b9cd81b24fbe0106fae5
mthanh2209 <mongthanh.220902@gmail.com>
2023-12-25 15:39:26 +0700

    Update get random color #103

--- user-manager/src/helpers/getRandomColor.ts
+++ user-manager/src/helpers/getRandomColor.ts
@@ -1,3 +1,8 @@
 export const getRandomColor = () => {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 };
