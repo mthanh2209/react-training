0618015d7c71c9b923ad871c041ead3c3460449f
mthanh2209 <mongthanh.220902@gmail.com>
2023-12-29 10:47:28 +0700

    Fix Item Nav Component #122

--- user-manager/src/components/DataDisplay/ItemNav/ItemNav.stories.tsx
+++ user-manager/src/components/DataDisplay/ItemNav/ItemNav.stories.tsx
@@ -3,10 +3,6 @@ import type { Meta, StoryObj } from '@storybook/react';
 // Components
 import ItemNav from '@components/DataDisplay/ItemNav/index';
 
-// Icons
-import userIcon from '@assets/images/user-icon.svg';
-import userIconSelected from '@assets/images/user-icon-selected.svg';
-
 export default {
   title: 'Components/ItemNav',
   component: ItemNav
@@ -16,8 +12,6 @@ type Story = StoryObj<typeof ItemNav>;
 
 export const Default: Story = {
   args: {
-    icon: userIcon,
-    iconSelected: userIconSelected,
     content: 'Users',
     isSelected: false
   }
