5aa91f35d8a0bad5b7487cd4fb8db54ae895dd15
mthanh2209 <mongthanh.220902@gmail.com>
2024-01-03 09:38:15 +0700

    Add comments for hooks folder #126

--- user-manager/src/hooks/useBackDrop.ts
+++ user-manager/src/hooks/useBackDrop.ts
@@ -4,9 +4,20 @@ import {
 } from 'react';
 
 interface IBackDropProps {
+  /**
+   * Reference to the HTMLElement used to determine clicks outside of its boundaries.
+   */
   ref: RefObject<HTMLElement>
+  /**
+   * Callback function invoked when a click occurs outside the referenced element.
+   */
   callback: () => void
 }
+
+/**
+ * Hook that handles click events outside a specified element.
+ * @param param0 - Object containing ref and callback properties.
+ */
 const useBackDrop = ({
   ref,
   callback
