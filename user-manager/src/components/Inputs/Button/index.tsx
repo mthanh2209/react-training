20bb359c6689c54a2862f5ac1914e866e1d1cac4
mthanh2209 <mongthanh.220902@gmail.com>
2024-01-03 17:52:42 +0700

    Remove unused button with icon #142

--- user-manager/src/components/Inputs/Button/index.tsx
+++ user-manager/src/components/Inputs/Button/index.tsx
@@ -2,26 +2,21 @@
 import '@components/Inputs/Button/Button.css'
 
 // Types
-type TVariant = 'primary' | 'secondary' | 'withIcon';
+type TVariant = 'primary' | 'secondary';
 type TType = 'button' | 'submit';
-type TWithIcon = 'none' | 'block';
 
 export interface IButtonProps {
   variants?: TVariant;
-  withIcon?: TWithIcon;
   type?: TType;
   form?: string;
-  icon?: string;
   content?: string;
   onClick?: () => void
 }
 
 const Button = ({
   variants,
-  withIcon,
   type,
   form,
-  icon,
   content,
   onClick
 }: IButtonProps) => (
@@ -30,12 +25,6 @@ const Button = ({
     type={type}
     form={form}
     onClick={onClick}>
-    <span style={{ display: `${withIcon}` }} className='icon-wrapper'>
-      <img
-        className='icon'
-        src={icon}
-        alt="icon" />
-    </span>
     {content}
   </button >
 );
