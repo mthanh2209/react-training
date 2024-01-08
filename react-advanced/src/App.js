a836ecf9e3788590bd1127c7a979e12383e08bb7
mthanh2209 <mongthanh.220902@gmail.com>
2024-01-08 17:33:16 +0700

    Implement sharing state #151

--- react-advanced/src/App.js
+++ react-advanced/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { InputState } from "./useState/inputState";
+import { SyncedInputs } from "./useState/sharingState/syncedInputs";
 import { MailStar } from "./useState/stateStructure/duplicationState";
 import { MailClient } from "./useState/stateStructure/multipleSelection";
 import { TravelPlan } from './useState/stateStructure/redundantState';
@@ -11,6 +12,7 @@ function App() {
       <TravelPlan/>
       <MailStar/>
       <MailClient/>
+      <SyncedInputs/>
     </>
   )
 }
