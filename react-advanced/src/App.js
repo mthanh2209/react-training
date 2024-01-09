e555aed10c7d2295e281b6b7aecba300d3639fcb
mthanh2209 <mongthanh.220902@gmail.com>
2024-01-09 09:41:05 +0700

    Initial router dom

--- react-advanced/src/App.js
+++ react-advanced/src/App.js
@@ -1,18 +1,39 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 import { InputState } from "./useState/inputState";
 import { MailStar } from "./useState/stateStructure/duplicationState";
 import { MailClient } from "./useState/stateStructure/multipleSelection";
-import { TravelPlan } from './useState/stateStructure/redundantState';
+import { TravelPlan } from "./useState/stateStructure/redundantState";
 
-function App() {
+const App = () => {
   return (
-    <>
-      <InputState/>
-      <TravelPlan/>
-      <MailStar/>
-      <MailClient/>
-    </>
-  )
-}
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/inputState">Input State</Link>
+          </li>
+          <li>
+            <Link to="/structureState">Structure State</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/inputState" element={<InputState />} />
+        <Route
+          path="/structureState"
+          element={
+            <>
+              <TravelPlan />
+              <MailStar />
+              <MailClient />
+            </>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
