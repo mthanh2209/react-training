e4e3e3830eccade3092d6c459dbd0bae27e4b497
mthanh2209 <mongthanh.220902@gmail.com>
2024-01-10 10:39:44 +0700

    Dispatch actions from handlers #153

---
+++ react-advanced/src/useReducer/messenger.jsx
@@ -0,0 +1,106 @@
+import { useReducer } from "react";
+
+const contacts = [
+  { id: 0, name: "Taylor", email: "taylor@mail.com" },
+  { id: 1, name: "Alice", email: "alice@mail.com" },
+  { id: 2, name: "Bob", email: "bob@mail.com" },
+];
+
+const initialState = {
+  selectedId: 0,
+  message: "Hello",
+};
+
+const messengerReducer = (state, action) => {
+  switch (action.type) {
+    case "changed_selection": {
+      return {
+        ...state,
+        selectedId: action.contactId,
+        message: "",
+      };
+    }
+    case "edited_message": {
+      return {
+        ...state,
+        message: action.message,
+      };
+    }
+    default: {
+      throw Error("Unknown action: " + action.type);
+    }
+  }
+};
+
+export const Messenger = () => {
+  const [state, dispatch] = useReducer(messengerReducer, initialState);
+  const message = state.message;
+  const contact = contacts.find((c) => c.id === state.selectedId);
+  return (
+    <div>
+      <ContactList
+        contacts={contacts}
+        selectedId={state.selectedId}
+        dispatch={dispatch}
+      />
+      <Chat
+        key={contact.id}
+        message={message}
+        contact={contact}
+        dispatch={dispatch}
+      />
+    </div>
+  );
+};
+
+const ContactList = ({
+  contacts,
+  selectedId,
+  dispatch
+}) => {
+  return (
+    <section className="contact-list">
+      <ul>
+        {contacts.map((contact) => (
+          <li key={contact.id}>
+            <button
+              onClick={() => {
+                dispatch({
+                  type: "changed_selection",
+                  contactId: contact.id,
+                });
+              }}
+            >
+              {selectedId === contact.id
+                ? <b>{contact.name}</b>
+                : contact.name
+              }
+            </button>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+const Chat = ({
+  contact,
+  message,
+  dispatch
+}) => {
+  return (
+    <section className="chat">
+      <textarea
+        value={message}
+        placeholder={"Chat to " + contact.name}
+        onChange={(e) => {
+          dispatch({
+            type: "edited_message",
+            message: e.target.value,
+          });
+        }}
+      />
+      <button>Send to {contact.email}</button>
+    </section>
+  );
+};
